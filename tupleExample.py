0de758e0cba008b9d1c4fb3451c6b5ede9566bf1
shailendertamta <shailendertamta2@gmail.com>
2024-11-13 07:50:33 +0000

    practising tuple in python

---
+++ tupleExample.py
@@ -0,0 +1,5 @@
+#Tuple Example
+# they are defined in () and are immutable i.e. it can not be changed once it is created #
+t = (1,2,3)
+print(t)
+#t.insert() there is no method as insert with tuple.
